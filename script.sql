7ac3ac560b178654027a984440481c7043124b47
bladimirlpz <bladimir.lopez.p@gmail.com>
2024-06-21 16:58:12 -0400

    modificaciones de tavla usuarios

--- script.sql
+++ script.sql
@@ -9,7 +9,7 @@ CREATE TABLE Usuarios (
     id SERIAL PRIMARY KEY,
     nombre VARCHAR(100) NOT NULL,
     apellido VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
     contrasena VARCHAR(255) NOT NULL
 );
 
